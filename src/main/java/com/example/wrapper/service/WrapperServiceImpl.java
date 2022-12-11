1a5529eed9aff25400a791e7e317f84921e59c02
Albert <albert.engalychev@gmail.com>
2022-12-11 17:27:23 +0300

    Optimize

--- src/main/java/com/example/wrapper/service/WrapperServiceImpl.java
+++ src/main/java/com/example/wrapper/service/WrapperServiceImpl.java
@@ -6,20 +6,23 @@ import java.lang.reflect.Constructor;
 
 public class WrapperServiceImpl implements WrapperService {
 
-    private final Class<? extends AbstractWrapperModel<?>> wrapperModel;
+    private final Constructor<? extends AbstractWrapperModel<?>> ctor;
 
     @SuppressWarnings("unchecked")
     public WrapperServiceImpl(AbstractWrapperModel<?> wrapperModel) {
-        this.wrapperModel = (Class<AbstractWrapperModel<?>>) wrapperModel.getClass();
+        try {
+            this.ctor = (Constructor<? extends AbstractWrapperModel<?>>) wrapperModel.getClass().getDeclaredConstructor();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public <T> AbstractWrapperModel<T> getWrapperModel(T data) {
-        AbstractWrapperModel<T> model;
+        AbstractWrapperModel<T> model = null;
 
         try {
-            Constructor<?> ctor  = wrapperModel.getDeclaredConstructor();
             model = (AbstractWrapperModel<T>) ctor.newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
